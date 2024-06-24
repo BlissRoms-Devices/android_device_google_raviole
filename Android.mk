bc78c3abf04797a6125bf14797ba9567e069ba76
Michael Bestas <mkbestas@lineageos.org>
2024-06-24 03:19:35 +0300

    raviole: Declare DM libs as symlinks during extraction
    Change-Id: Icb6c2c284037f67478c80725f63b4a7db7c552c0
    Signed-off-by: Jis G Jacob <studiokeys@blissroms.org>

--- Android.mk
+++ Android.mk
@@ -40,21 +40,3 @@ $(eval $(call declare-1p-copy-files,device/google/raviole,audio_policy_configura
 # if some modules are built directly from this directory (not subdirectories),
 # their rules should be written here.
 
-ifeq ($(USES_DEVICE_GOOGLE_RAVIOLE),true)
-DM_LIBS := libdmengine.so libdmjavaplugin.so
-DM_32_SYMLINKS := $(addprefix $(TARGET_OUT_PRODUCT)/priv-app/DMService/lib/arm/,$(notdir $(DM_LIBS)))
-$(DM_32_SYMLINKS): $(LOCAL_INSTALLED_MODULE)
-	@echo "DMService 32 lib link: $@"
-	@mkdir -p $(dir $@)
-	@rm -rf $@
-	$(hide) ln -sf /product/lib/$(notdir $@) $@
-
-DM_64_SYMLINKS := $(addprefix $(TARGET_OUT_PRODUCT)/priv-app/DMService/lib/arm64/,$(notdir $(DM_LIBS)))
-$(DM_64_SYMLINKS): $(LOCAL_INSTALLED_MODULE)
-	@echo "DMService 64 lib link: $@"
-	@mkdir -p $(dir $@)
-	@rm -rf $@
-	$(hide) ln -sf /product/lib64/$(notdir $@) $@
-
-ALL_DEFAULT_INSTALLED_MODULES += $(DM_32_SYMLINKS) $(DM_64_SYMLINKS)
-endif
